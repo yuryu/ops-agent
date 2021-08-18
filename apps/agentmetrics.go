6dabbfae67a832e37c9355584c57d1e2a9b8e972
Quentin Smith <quentin@google.com>
2021-08-18 14:55:06 -0400

    Checkpoint

--- apps/agentmetrics.go
+++ apps/agentmetrics.go
@@ -12,7 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-package confgenerator
+package apps
 
 import "github.com/GoogleCloudPlatform/ops-agent/confgenerator/otel"
 
